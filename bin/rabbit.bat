0a30bc1f8d6320357e89e75278524b6c3cfeb684
Kouhei Sutou <kou@cozmixng.org>
2004-10-10 06:35:06 +0000

    * added `--log-type gui' option.

--- bin/rabbit.bat
+++ bin/rabbit.bat
@@ -1,8 +1,8 @@
 @echo off
 if "%OS%" == "Windows_NT" goto WinNT
-start rubyw -S rabbit %1 %2 %3 %4 %5 %6 %7 %8 %9
+start rubyw -S rabbit --log-type gui %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto endofruby
 :WinNT
-start rubyw -S rabbit %*
+start rubyw -S rabbit --log-type gui %*
 goto endofruby
 :endofruby
