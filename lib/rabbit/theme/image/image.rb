6d6b3b70726f04878a2dd74b8811956ef5856513
Kouhei Sutou <kou@cozmixng.org>
2005-06-29 07:49:49 +0000

    * supported image with frame.

--- lib/rabbit/theme/image/image.rb
+++ lib/rabbit/theme/image/image.rb
@@ -1,4 +1,20 @@
+proc_name = "image"
+
+if @image_with_frame.nil?
+#  @image_with_frame = true
+end
+
+@image_frame_color ||= "black"
+@image_frame_shadow_color ||= "gray"
+@image_frame_padding ||= 3
+@image_frame_shadow_width ||= 3
+@image_frame_shadow_offset ||= 2
+
 match("**", Image) do |images|
+
+  images.delete_pre_draw_proc_by_name(proc_name)
+  images.delete_post_draw_proc_by_name(proc_name)
+  
   images.horizontal_centering = true
 
   space = screen_size(3)
@@ -12,7 +28,7 @@ match("**", Image) do |images|
   end
 
   images.each do |image|
-    image.add_pre_draw_proc do |canvas, x, y, w, h, simulation|
+    image.add_pre_draw_proc(proc_name) do |canvas, x, y, w, h, simulation|
       nw = make_normalized_size.call(image.normalized_width)
       nh = make_normalized_size.call(image.normalized_height)
       rw = make_relative_size.call(image.relative_width, w)
@@ -20,6 +36,30 @@ match("**", Image) do |images|
       iw = nw || rw
       ih = nh || rh
       image.resize(iw, ih)
+
+      if @image_with_frame and !simulation
+        # Frame
+        f_sx = x - @image_frame_padding
+        f_sy = y - @image_frame_padding
+        f_ex = image.width + 2 * @image_frame_padding
+        f_ey = image.height + 2 * @image_frame_padding
+        canvas.draw_rectangle(false, f_sx, f_sy, f_ex, f_ey, "black")
+
+        # Under Shadow
+        us_sx = f_sx + @image_frame_shadow_offset
+        us_sy = f_sy + f_ey + 1
+        us_ex = f_ex + @image_frame_shadow_width - @image_frame_shadow_offset
+        us_ey = @image_frame_shadow_width
+        canvas.draw_rectangle(true, us_sx, us_sy, us_ex, us_ey, "gray")
+        
+        # Right Shadow
+        rs_sx = f_sx + f_ex + 1
+        rs_sy = f_sy + @image_frame_shadow_offset
+        rs_ex = @image_frame_shadow_width
+        rs_ey = f_ey + @image_frame_shadow_width - @image_frame_shadow_offset
+        canvas.draw_rectangle(true, rs_sx, rs_sy, rs_ex, rs_ey, "gray")
+      end
+      
       [x, y, w, h]
     end
   end
@@ -27,7 +67,7 @@ match("**", Image) do |images|
   images.each do |image|
     layout = nil
     th = 0
-    image.add_post_draw_proc do |canvas, x, y, w, h, simulation|
+    image.add_post_draw_proc(proc_name) do |canvas, x, y, w, h, simulation|
       if image.caption and simulation
         caption = NormalText.new(image.caption)
         caption.prop_set("size", @normal_font_size)
@@ -40,7 +80,11 @@ match("**", Image) do |images|
         th = caption.height
       end
       if !simulation and layout
-        canvas.draw_layout(layout, image.ox || x, y) # dirty!!!
+        base_y = y
+        if @image_with_frame
+          base_y += @image_frame_shadow_offset + @image_frame_shadow_width
+        end
+        canvas.draw_layout(layout, image.ox || x, base_y) # dirty!!!
       end
       [x, y + space + th, w, h - space - th]
     end
