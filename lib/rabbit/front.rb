0bb47ef76a87e2d6ab9cc9828b9d1d8235db6579
Kouhei Sutou <kou@cozmixng.org>
2005-07-16 11:33:22 +0000

    * minor fix.

--- lib/rabbit/front.rb
+++ lib/rabbit/front.rb
@@ -11,34 +11,33 @@ module Rabbit
     attr_reader :image_type
     
     def initialize(canvas)
-      @original_canvas = canvas
-      @canvas = canvas.offline_screen_canvas
+      @canvas = canvas
       @last_modified = Time.at(0)
-      @images = []
       @image_type = "png"
       @mutex = Mutex.new
+      reset
     end
 
     def current_page_image
       update_images_if_need
-      @images[@original_canvas.current_index]
+      @images[@canvas.current_index]
     end
 
     def total_page_number
-      @original_canvas.slide_size
+      @canvas.slide_size
     end
 
     def current_page_number
-      @original_canvas.current_index + 1
+      @canvas.current_index + 1
     end
 
     private
     def update_images_if_need
       @mutex.synchronize do
-        @images = [] if dirty?
-        index = @original_canvas.current_index
+        reset if dirty?
+        index = @canvas.current_index
         if @images[index].nil?
-          pixbuf = @canvas.to_pixbuf(index)
+          pixbuf = @offline_screen_canvas.to_pixbuf(index)
           @images[index] = pixbuf.save_to_buffer(@image_type)
           synchronized
         end
@@ -51,6 +50,11 @@ module Rabbit
 
     def synchronized
       @last_modified = @canvas.last_modified
+    end
+
+    def reset
+      @offline_screen_canvas = @canvas.offline_screen_canvas
+      @images = [] 
     end
   end
 end
