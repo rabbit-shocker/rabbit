282e7fe4c8dccaab7708dab236f1c33fa8c8c4f3
Kouhei Sutou <kou@cozmixng.org>
2005-08-25 21:36:05 +0000

    * use #height.

--- lib/rabbit/theme/headline-logo/headline-logo.rb
+++ lib/rabbit/theme/headline-logo/headline-logo.rb
@@ -9,30 +9,19 @@ match("**", HeadLine) do |heads|
   loader = ImageLoader.new(search_file(@headline_logo_image))
 
   resized = false
-  margin = screen_y(3)
 
   heads.each do |head|
-    start_y = end_y = nil
-    
     head.delete_pre_draw_proc_by_name(proc_name)
     head.delete_post_draw_proc_by_name(proc_name)
 
     head.add_pre_draw_proc(proc_name) do |canvas, x, y, w, h, simulation|
       unless simulation
         unless resized
-          header_height = end_y - start_y - margin
-          width = ((header_height / loader.height.to_f) * loader.width).ceil
-          loader.resize(width, header_height) if width > 0
+          loader.resize(nil, head.height)
           resized = true
         end
         canvas.draw_pixbuf(loader.pixbuf, w - loader.width, y)
       end
-      start_y = y
-      [x, y, w, h]
-    end
-    
-    head.add_post_draw_proc(proc_name) do |canvas, x, y, w, h, simulation|
-      end_y = y
       [x, y, w, h]
     end
   end
