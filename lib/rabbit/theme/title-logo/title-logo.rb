9e8d3d369b4324137b5ae20d3e71812e8ba45253
Kouhei Sutou <kou@cozmixng.org>
2005-10-25 16:09:15 +0000

    * logo position can be changed.

--- lib/rabbit/theme/title-logo/title-logo.rb
+++ lib/rabbit/theme/title-logo/title-logo.rb
@@ -4,11 +4,13 @@ if @title_logo_image.nil?
   theme_exit("must specify @title_logo_image!!")
 end
 
+@title_logo_position ||= :right
+
 match(TitleSlide, Title) do |titles|
 
   titles.delete_pre_draw_proc_by_name(proc_name)
 
-  break if @title_log_image_uninstall
+  break if @title_logo_image_uninstall
   
   loader = ImageLoader.new(find_file(@title_logo_image))
 
@@ -23,7 +25,11 @@ match(TitleSlide, Title) do |titles|
         loader.resize(width, title_space) if width > 0
         resized = true
       end
-      new_x = canvas.width - loader.width - title_slide.margin_right
+      if @title_logo_position == :right
+        new_x = canvas.width - loader.width - title_slide.margin_right
+      else
+        new_x = title_slide.margin_left
+      end
       new_y = title_slide.margin_top
       canvas.draw_pixbuf(loader.pixbuf, new_x, new_y)
     end
