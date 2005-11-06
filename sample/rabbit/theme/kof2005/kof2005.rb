184f742bb370abad50559099de9d272e1e56dc36
Kouhei Sutou <kou@cozmixng.org>
2005-11-06 10:42:44 +0000

    * fixed turtoise position.

--- sample/rabbit/theme/kof2005/kof2005.rb
+++ sample/rabbit/theme/kof2005/kof2005.rb
@@ -1,9 +1,9 @@
 @huge_font_size = (@huge_font_size * 0.8).ceil
 
+include_theme("ruby-gnome2")
+
 unless print?
   @image_timer_image = "mini-kame-taro.png"
   @image_timer_limit ||= 15 * 60
   include_theme("image-timer")
 end
-
-include_theme("ruby-gnome2")
