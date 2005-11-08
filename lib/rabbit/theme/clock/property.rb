eac1b6826f152642c3449646534b4291afe54f65
Kobayashi Noritada <noritadak@gmail.com>
2005-11-08 20:04:57 +0000

    * Describe property.rb for the 'clock' theme.

--- lib/rabbit/theme/clock/property.rb
+++ lib/rabbit/theme/clock/property.rb
@@ -1,3 +1,19 @@
 @category = N_("Time")
 @name = N_("TextClock")
-@description = N_("text version clock")
+@abstract = N_("Clock, a text version")
+@description = N_("Display current time with text.")
+@parameters = {
+  "@clock_auto_update" => {
+    :default => "true",
+    :description => "Whether update clock automatically or not.",
+  },
+  "@clock_props" => {
+    :default => "{'size' => @xx_small_font_size, " \
+                "'font_family' => @font_family}",
+    :description => N_("Properties for clocks, such as font family."),
+  },
+  "@clock_uninstall" => {
+    :default => "nil",
+    :description => N_("Whether uninstall clock or not."),
+  },
+}
