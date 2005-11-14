ffdbabda7c429c6f8f0f97ddfd8c7fd14ae58bce
Kobayashi Noritada <noritadak@gmail.com>
2005-11-14 03:05:51 +0000

    * Remove unnecessary N_() markings from property.rb for the 'show-frame'   theme.

--- lib/rabbit/theme/show-frame/property.rb
+++ lib/rabbit/theme/show-frame/property.rb
@@ -4,11 +4,11 @@
 @description = N_("Show element frames useful for debug.")
 @parameters = {
   "@show_frame_color" => {
-    :default => N_("'blue'"),
+    :default => "'blue'",
     :description => N_("Frame color."),
   },
   "@show_frame_uninstall" => {
-    :default => N_("nil"),
+    :default => "nil",
     :description => N_("Whether uninstalling this toolkit or not. " \
                        "This option is useful for cases you do not want to " \
                        "make the toolkit work for certain slides."),
