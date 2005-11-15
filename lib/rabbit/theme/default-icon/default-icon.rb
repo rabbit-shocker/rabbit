d4077dd527764ea90362164649f5188f1b0bc44b
Kouhei Sutou <kou@cozmixng.org>
2005-11-15 13:40:47 +0000

    * added small version icons. * use small icon too. Suggested by nori.

--- lib/rabbit/theme/default-icon/default-icon.rb
+++ lib/rabbit/theme/default-icon/default-icon.rb
@@ -1,4 +1,4 @@
 add_theme_path("ruby-images")
 
-@icon_images = ["ruby-pink-circle-icon.png", "ruby-pink-polygon-icon.png"]
+@icon_images = ["ruby-pink-circle-small-icon.png", "ruby-pink-polygon-icon.png"]
 include_theme("icon")
