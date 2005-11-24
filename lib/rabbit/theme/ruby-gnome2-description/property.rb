4f70106c14862dd00a82a2ede346ff2e35120f8a
Kobayashi Noritada <noritadak@gmail.com>
2005-11-24 08:29:40 +0000

    * Improve property.rb for three themes, 'ruby-gnome2-description',   'ruby-gnome2-item-mark', and 'ruby-gnome2-headline'.

--- lib/rabbit/theme/ruby-gnome2-description/property.rb
+++ lib/rabbit/theme/ruby-gnome2-description/property.rb
@@ -3,5 +3,5 @@
 @abstract = N_("Toolkit to display description-list items like ones in the " \
                "website of the Ruby-GNOME2 Project")
 @description = N_("Displays description-list items like ones in the website " \
-                  "of the Ruby-GNOME2 Project; i.e. displays them colored " \
-                  "blue.")
+                  "of the Ruby-GNOME2 Project; i.e. displays their text " \
+                  "colored blue.")
--- lib/rabbit/theme/ruby-gnome2-headline/property.rb
+++ lib/rabbit/theme/ruby-gnome2-headline/property.rb
@@ -3,5 +3,5 @@
 @abstract = N_("Toolkit to display headlines like those in the website of " \
                "the Ruby-GNOME2 Project")
 @description = N_("Displays headlines like ones in the website of the " \
-                  "Ruby-GNOME2 Project; i.e. displays them colored white " \
-                  "and in the dark red boxes with pink frames.")
+                  "Ruby-GNOME2 Project; i.e. displays their text colored " \
+                  "white and placed in the dark red boxes with pink frames.")
