676c2ed200991a91028f3f98abfb0223d47bc440
Kouhei Sutou <kou@cozmixng.org>
2005-12-01 14:23:37 +0000

    * simple view.

--- lib/rabbit/theme/browser.rb
+++ lib/rabbit/theme/browser.rb
@@ -10,11 +10,24 @@ module Rabbit
       
       TREE_MODEL = [
         [:name, String],
+        [:localized_name, String],
+        [:type, String],
       ]
+
+			TAG_INFOS = [
+				[
+					"category",
+					{
+						"weight" => Pango::FontDescription::WEIGHT_BOLD,
+						"size" => 15 * Pango::SCALE,
+					}
+				]
+			]
       
       def initialize(locales=nil)
         @locales = locales
         @themes = Rabbit::Theme::Searcher.collect_theme
+				@buffers = {}
         init_gui
         load_themes
       end
@@ -70,14 +83,25 @@ module Rabbit
 
       def init_tree_columns
         renderer = Gtk::CellRendererText.new
-        @tree.insert_column(-1, _("Name"), renderer, "text" => column(:name))
+        @tree.insert_column(-1, _("Theme"), renderer,
+														"text" => column(:localized_name))
+				@tree.selection.signal_connect("changed") do |selection|
+					iter = selection.selected
+					theme_changed(iter) if iter
+				end
       end
+
+			def theme_changed(iter)
+				case iter.get_value(column(:type))
+				when "theme"
+					update_buffer(iter.get_value(column(:name)))
+				end
+			end
 
       def init_document_view
         @view = Gtk::TextView.new
         @hpaned.add2(@view)
         @view.wrap_mode = Gtk::TextTag::WRAP_WORD
-        @buffer = @view.buffer
       end
 
       def column(key)
@@ -85,25 +109,48 @@ module Rabbit
       end
 
       def load_themes
-        themes = Searcher.collect_theme do |entry|
+        @themes = Searcher.collect_theme do |entry|
           Utils.process_pending_events
         end
         
         model = @tree.model
         categories = {}
-        themes.each do |entry|
-          category = _(entry.category)
+        @themes.each do |entry|
+          category = entry.category
           iter = categories[category]
           if iter.nil?
             iter = model.append(nil)
             categories[category] = iter
+            iter[column(:type)] = "category"
             iter[column(:name)] = category
+            iter[column(:localized_name)] = _(category)
           end
           child_iter = model.append(iter)
-          child_iter[column(:name)] = _(entry.name)
+					child_iter[column(:type)] = "theme"
+          child_iter[column(:name)] = entry.name
+          child_iter[column(:localized_name)] = _(entry.name)
           Utils.process_pending_events
         end
+				@tree.expand_all
       end
+
+			def init_tags(buffer)
+				TAG_INFOS.each do |name, properties|
+					buffer.create_tag(name, properties)
+				end
+			end
+
+			def update_buffer(name)
+				buffer = @buffers[name]
+				if buffer.nil?
+					buffer = Gtk::TextBuffer.new
+					init_tags(buffer)
+					@buffers[name] = buffer
+				end
+				@view.buffer = buffer
+				entry = @themes.find {|entry| entry.name == name}
+				buffer.text = "#{_(entry.name)}\n#{_(entry.category)}"
+			end
     end
   end
 end
