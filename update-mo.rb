f5dd74d56560aacd5dbc93feb841b10645998c19
Kouhei Sutou <kou@cozmixng.org>
2005-12-29 11:39:33 +0000

    * updated message.

--- update-mo.rb
+++ update-mo.rb
@@ -2,7 +2,7 @@
 
 require 'fileutils'
 
-msgfmt = "ruby -S rmsgfmt"
+msgfmts = ["msgfmt", "ruby -S rmsgfmt"]
 
 podir = "po/"
 modir = "data/locale/%s/LC_MESSAGES/"
@@ -11,8 +11,10 @@ Dir.glob("#{podir}*/*.po") do |file|
   _, lang, basename = file.sub(/\.po$/, '').split(File::SEPARATOR)
   outdir = modir % lang
   FileUtils.mkdir_p(outdir) unless File.directory?(outdir)
-  command = "#{msgfmt} #{file} -o #{outdir}#{basename}.mo"
-  unless system(command)
-    STDERR.puts("Can't run: #{command}")
+  msgfmts.each do |msgfmt|
+    command = "#{msgfmt} #{file} -o #{outdir}#{basename}.mo"
+    unless system(command)
+      STDERR.puts("Can't run: #{command}")
+    end
   end
 end
