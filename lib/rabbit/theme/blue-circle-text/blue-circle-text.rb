e36db9dee11060154fe45033694bd143fb01eefd
Kouhei Sutou <kou@cozmixng.org>
2006-01-29 05:58:22 +0000

    * added indent configurations.

--- lib/rabbit/theme/blue-circle-text/blue-circle-text.rb
+++ lib/rabbit/theme/blue-circle-text/blue-circle-text.rb
@@ -3,3 +3,7 @@ include_theme("default-text")
 match(Slide, "*") do |elems|
 #  elems.prop_set("weight", "bold")
 end
+
+match(Slide, Body, Paragraph) do |texts|
+  texts.margin_left = canvas.width * 0.15
+end
--- lib/rabbit/theme/blue-circle-title-slide/blue-circle-title-slide.rb
+++ lib/rabbit/theme/blue-circle-title-slide/blue-circle-title-slide.rb
@@ -4,11 +4,24 @@ match(TitleSlide) do |slides|
   blue_circle_slide(slides, "title-slide")
 end
 
+match(TitleSlide, "*") do |elements|
+  elements.each do |element|
+    element.margin_left = canvas.width * 0.2
+    element.margin_right = canvas.width * 0.05
+    unless [Title, Subtitle, Author].find {|type| element.is_a?(type)}
+      element.align = Pango::Layout::ALIGN_RIGHT
+    end
+  end
+end
+
 match(TitleSlide, Title) do |titles|
   blue_circle_title(titles, 'title')
 end
 
-match(TitleSlide, Subtitle) do |titles|
-  titles.margin_left = canvas.width * 0.2
-  titles.margin_right = canvas.width * 0.05
+match(TitleSlide, Author) do |authors|
+  authors.margin_top = @space * 3
+end
+
+match(TitleSlide, Institution) do |institutions|
+  institutions.margin_top = @space
 end
