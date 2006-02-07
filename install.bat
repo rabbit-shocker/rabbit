d23168bea24c808b16305fd7423976d3ab89cbcf
MIYAMUKO Katsuyuki <miyamuko@gmail.com>
2006-02-07 09:02:37 +0000

    - added post install check.

--- install.bat
+++ install.bat
@@ -1,5 +1,19 @@
+@echo off
+
+@echo ruby setup.rb
+
+rem run installer
 ruby setup.rb
-if not %errorlevel%==0 goto end
+if errorlevel 1 goto error
+
+rem install check & show welcome message
+ruby -rrabbit/rabbit -e "puts \"*** Successfully installed Rabbit #{Rabbit::VERSION}!\""
+if errorlevel 1 goto error
+
+goto end
+
+:error
+@echo *** Failed to install Rabbit... (error code: %errorlevel%)
 
 :end
 pause
