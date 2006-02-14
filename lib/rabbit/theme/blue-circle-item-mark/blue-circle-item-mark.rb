50339430a1dd898669183147a8a58f3f986a6036
Kouhei Sutou <kou@cozmixng.org>
2006-02-14 14:38:39 +0000

    * supported ordered list.

--- lib/rabbit/theme/blue-circle-item-mark/blue-circle-item-mark.rb
+++ lib/rabbit/theme/blue-circle-item-mark/blue-circle-item-mark.rb
@@ -1,3 +1,5 @@
+margin_left = canvas.width * 0.05
+
 slide_body = [Slide, Body]
 
 item_list_item = [ItemList, ItemListItem]
@@ -8,8 +10,7 @@ match(*(slide_body + (item_list_item * 1))) do |items|
   mark_width = screen_x(4)
   mark_height = screen_y(4)
   mark_space = mark_width * 0.3
-  margin_left = canvas.width * 0.05 + mark_width
-  indent_width = margin_left + mark_width + mark_space
+  indent_width = margin_left + mark_width * 2 + mark_space
 
   items.delete_pre_draw_proc_by_name(name)
   items.delete_post_draw_proc_by_name(name)
@@ -46,4 +47,72 @@ match(*(slide_body + (item_list_item * 2))) do |items|
 
   space = @space * (3 / 4.0)
   items.margin_bottom = space
+end
+
+enum_list_item = [EnumList, EnumListItem]
+
+match(*(slide_body + (enum_list_item * 1))) do |items|
+  name = "enum1"
+  
+  indent_width = margin_left + screen_x(2)
+  default_props = {
+    "size" => @normal_font_size,
+    "font_family" => @font_family,
+  }
+
+  delete_pre_draw_proc_by_name(name)
+  delete_post_draw_proc_by_name(name)
+  
+  draw_order(items, indent_width, name) do |item|
+    props = default_props
+    props = props.merge(item.first.text_props) unless item.empty?
+    %Q[<span #{to_attrs(props)}>#{item.order}. </span>]
+  end
+
+  space = @space * (3 / 4.0)
+  margin_with(:bottom => space)
+end
+
+match(*(slide_body + (enum_list_item * 2))) do |items|
+  name = "enum2"
+  
+  indent_width = screen_x(1.5)
+  default_props = {
+    "size" => @small_font_size,
+    "font_family" => @font_family,
+  }
+
+  delete_pre_draw_proc_by_name(name)
+  delete_post_draw_proc_by_name(name)
+  
+  draw_order(items, indent_width, name) do |item|
+    props = default_props
+    props = props.merge(item.first.text_props) unless item.empty?
+    %Q[<span #{to_attrs(props)}>#{(?a + item.order - 1).chr}. </span>]
+  end
+
+  space = @space * (2 / 4.0)
+  margin_with(:bottom => space)
+end
+
+match(*(slide_body + (enum_list_item * 3))) do |items|
+  name = "enum3"
+  
+  indent_width = screen_x(1)
+  default_props = {
+    "size" => @x_small_font_size,
+    "font_family" => @font_family,
+  }
+
+  delete_pre_draw_proc_by_name(name)
+  delete_post_draw_proc_by_name(name)
+  
+  draw_order(items, indent_width, name) do |item|
+    props = default_props
+    props = props.merge(item.first.text_props) unless item.empty?
+    %Q[<span #{to_attrs(props)}>#{(?A + item.order - 1).chr}. </span>]
+  end
+
+  space = @space * (1 / 4.0)
+  margin_with(:bottom => space)
 end
