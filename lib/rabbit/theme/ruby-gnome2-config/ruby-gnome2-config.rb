6118723b8803bca48b4ef285c441bcbac9987a69
Kouhei Sutou <kou@cozmixng.org>
2006-02-17 13:38:11 +0000

    * added emphasis color configuration.

--- lib/rabbit/theme/ruby-gnome2-config/ruby-gnome2-config.rb
+++ lib/rabbit/theme/ruby-gnome2-config/ruby-gnome2-config.rb
@@ -1,4 +1,5 @@
 @ruby_gnome2_color ||= "#003399"
+@ruby_gnome2_emphasis_color ||= "#006600"
 @ruby_gnome2_line_color ||= "#ff9999"
 @ruby_gnome2_frame_color ||= "#3399cc"
 @ruby_gnome2_fill_color ||= "#ddeeff"
