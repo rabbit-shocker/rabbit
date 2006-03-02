00480748fa593028ff6e3d2935035f4e6ab4db53
Kouhei Sutou <kou@cozmixng.org>
2006-03-02 02:16:49 +0000

    followup previous commit.
    * added base implementation for Display module.

---
+++ lib/rabbit/renderer/display/base.rb
@@ -0,0 +1,15 @@
+require "rabbit/renderer/base"
+
+module Rabbit
+  module Renderer
+    module Display
+      module Base
+        include Renderer::Base
+
+        private
+        def configured(x, y, w, h)
+        end
+      end
+    end
+  end
+end
