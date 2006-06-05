4185abfb84f598d5e00d60735dab4cd68fa68368
Kouhei Sutou <kou@cozmixng.org>
2006-06-05 15:55:24 +0000

    * cleanup inserting image code.

--- misc/emacs/rabbit-mode.el
+++ misc/emacs/rabbit-mode.el
@@ -1,9 +1,10 @@
-;;; -*- Emacs-Lisp -*-
+;;; -*- mode: Emacs-Lisp; indent-tabs-mode: nil -*-
 ;;; rabbit-mode.el
 ;;  Emacs major mode for Rabbit
 ;;; Copyright (c) 2006 Atsushi TAKEDA <tkdats@kono.cis.iwate-u.ac.jp>
 ;;; $Date$
 
+(require 'cl)
 (require 'rd-mode)
 
 (defvar rabbit-mode-hook nil
@@ -14,7 +15,7 @@
 (defvar rabbit-institution "Institution")
 (defvar rabbit-theme "rabbit")
 
-(defvar rabbit-title-template 
+(defvar rabbit-title-template
 "= %s
 
 : author
@@ -23,13 +24,6 @@
    %s
 : theme
    %s
-\n")
-
-(defvar rabbit-image-template 
-" # image
- # src = %s
-%s
-%s
 \n")
 
 (defvar rabbit-slide-template
@@ -75,15 +69,14 @@
       0 rabbit-comment-face)
    ))
 
-(defvar rabbit-specify-imagesize-list
-  '(("size")
-    ("normalized_width")
-    ("normalized_height")
-    ("relative_width")
-    ("relative_height")))
+(defvar rabbit-block-indent-size 2)
 
-(defvar rabbit-specify-imagesize-default
-  "relative-height")
+(defvar rabbit-image-size-unit-table
+  '(("relative")
+    ("normalized")
+    ("pixel")))
+
+(defvar rabbit-image-size-unit-history nil)
 
 (define-derived-mode rabbit-mode rd-mode "Rabbit"
   (setq-default rabbit-running nil)
@@ -123,25 +116,22 @@
 				  rabbit-theme)))
   (forward-line 9))
 
-(defun rabbit-insert-image-template (file cap)
+(defun rabbit-insert-image-template (file)
   "insert a image template."
-  (interactive "fimage file: \nscaption:")
-  (let ((size (rabbit-read-size)))
-    (rabbit-print-image-template file cap size)
-    (rabbit-move-after-insert-image size)))
+  (interactive "fimage file: ")
+  (rabbit-insert-image-template-real file (rabbit-read-size-unit)))
 
-(defun rabbit-insert-image-template-default (file cap)
-  "insert a image template with default way to specify size."
-  (interactive "fimage file: \nscaption:")
-  (rabbit-print-image-template file cap rabbit-specify-imagesize-default)
-  (rabbit-move-after-insert-image rabbit-specify-imagesize-default))
+(defun rabbit-insert-image-template-default (file)
+  "insert a image template with default image size unit."
+  (interactive "fimage file: ")
+  (rabbit-insert-image-template-real file))
 
 
 (defun rabbit-insert-slide (rabbit-slide-title)
   "insert a slide."
-  (interactive "sslide title:")
+  (interactive "sslide title: ")
   (save-excursion (insert (format rabbit-slide-template
-				  rabbit-slide-title)))
+                                  rabbit-slide-title)))
   (forward-line 2))
 
 ;;; move functions
@@ -170,7 +160,28 @@
   (define-key rabbit-mode-map "\C-c\C-s" 'rabbit-insert-slide)
   (define-key rabbit-mode-map "\M-n" 'rabbit-next-slide)
   (define-key rabbit-mode-map "\M-p" 'rabbit-previous-slide))
-  
+
+(defun rabbit-read-property (key)
+  "read `key' value from minibuf and return string as \"key = value\"
+format if value is specified, otherwise return \"\"."
+  (let ((value (read-from-minibuffer (concat key ": "))))
+    (if (string-equal value "")
+        ""
+      (concat key " = " value))))
+
+(defun rabbit-block-indent (string)
+  (let ((indent ""))
+    (dotimes (i rabbit-block-indent-size (concat indent string))
+      (setq indent (concat indent " ")))))
+
+(defun rabbit-read-block-property (key)
+  "read `key' value from minibuf and return string as \"key = value\"
+format if value is specified, otherwise return \"\"."
+  (let ((property (rabbit-read-property key)))
+    (if (string-equal property "")
+        ""
+      (rabbit-block-indent (concat "# " property)))))
+
 (defun rabbit-buffer-filename ()
   "return file name relating current buffer."
   (or (buffer-file-name)
@@ -188,44 +199,58 @@
 	 (buf (get-buffer-create bufname)))
     buf))
 
-(defun rabbit-read-size ()
-  "input function of way to specicfy size."
-  (completing-read "way to specicfy size:"
-		   rabbit-specify-imagesize-list))
+(defun rabbit-default-image-size-unit ()
+  (caar rabbit-image-size-unit-table))
 
-(defun rabbit-print-image-template (filename caption size)
-  "insert a image-template."
-  (let ((file (file-relative-name filename))
-	(cap (if (string-equal caption "")
-		 "# # caption ="
-	       (format " # caption = %s" caption)))
-	(size-string (rabbit-read-size-value-string size)))
-    (save-excursion (insert (format rabbit-image-template
-				    file
-				    cap
-				    size-string)))))
+(defun rabbit-read-size-unit ()
+  "read what unit use for image size."
+  (completing-read "image size unit: "
+		   rabbit-image-size-unit-table
+                   nil t nil
+                   'rabbit-image-size-unit-history
+                   (rabbit-default-image-size-unit)))
 
-(defun rabbit-read-size-value-string (size)
+(defun rabbit-filter (predicate lst)
+  (let ((result '()))
+    (dolist (item lst (reverse result))
+      (unless (funcall predicate item)
+        (setq result (cons item result))))))
+
+(defun rabbit-not-empty-string (string)
+  (and string (not (string-equal string ""))))
+
+(defun rabbit-join-without-empty-string (strings separator)
+  (let ((result "")
+        (not-empty-strings (rabbit-filter
+                            '(lambda (string)
+                               (not (rabbit-not-empty-string string)))
+                            strings)))
+    (if (null not-empty-strings)
+        ""
+      (dolist (string (cdr not-empty-strings)
+                      (concat (car not-empty-strings) result))
+        (setq result (concat result separator string))))))
+
+(defun rabbit-insert-image-template-real (filename &optional unit)
+  "insert a image template."
+  (insert (concat
+           (rabbit-join-without-empty-string
+            `(,(rabbit-block-indent "# image")
+              ,(rabbit-block-indent (concat "# src = "
+                                            (file-relative-name filename)))
+              ,(rabbit-read-block-property "caption")
+              ,@(rabbit-read-size-value
+                 (or unit (rabbit-default-image-size-unit))))
+            "\n")
+           "\n")))
+
+(defun rabbit-read-size-value (unit)
   "return strings that specify image size."
-  (cond
-   ((string-equal size "")
-    (format ""))
-   ((string-equal size "size")
-    (format " # width = %s\n # height = %s"
-	    (read-from-minibuffer "width:")
-	    (read-from-minibuffer "height:")))
-   (t
-     (format " # %s = %s"
-	     size
-	     (read-from-minibuffer (format "%s:" size))))))
+  (let ((prefix (if (string-equal unit "pixel")
+                    ""
+                  (concat unit "_"))))
+    (mapcar '(lambda (key)
+               (rabbit-read-block-property (concat prefix key)))
+            '("width" "height"))))
 
-(defun rabbit-move-after-insert-image (size)
-  "move to next location after insert image."
-  (cond ((string-equal size "")
-	 (forward-line 4))
-	((string-equal size "size")
-	 (forward-line 6))
-	(t
-	 (forward-line 5))))
-  
 (provide 'rabbit-mode)
