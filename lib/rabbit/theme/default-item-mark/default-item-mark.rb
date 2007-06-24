440112f7a1c72e771e60f2138b065a3509409a33
Kouhei Sutou <kou@cozmixng.org>
2007-06-24 02:54:30 +0000

    * cleanup default-item-mark theme.

--- lib/rabbit/theme/default-item-mark/default-item-mark.rb
+++ lib/rabbit/theme/default-item-mark/default-item-mark.rb
@@ -1,177 +1,102 @@
+@default_item1_mark_color ||= "green"
+@default_item2_mark_color ||= "blue"
+@default_item3_mark_color ||= "red"
+@default_enum_item1_mark_color ||= "#00ffff"
+@default_enum_item2_mark_color ||= "#ff00ff"
+
+def setup_default_item_mark(items, name, width, height, space_ratio, color)
+  mark_width = screen_x(width)
+  mark_height = screen_y(height)
+  indent_width = mark_width * 3
+
+  delete_pre_draw_proc_by_name(name)
+  delete_post_draw_proc_by_name(name)
+
+  args = [items, indent_width, mark_width, mark_height, name]
+  draw_mark(*args) do |item, canvas, x, y, w, h|
+    x -= mark_width
+    if block_given?
+      yield(item, canvas, x, y, w, h, color)
+    else
+      canvas.draw_rectangle(true, x, y, w, h, color)
+    end
+  end
+
+  space = @space * space_ratio
+  margin_with(:bottom => space)
+end
+
+def setup_default_enum_item_mark(items, level, indent, space_ratio, props)
+  name = "enum#{level}"
+
+  indent_width = screen_x(2)
+  default_props = {
+    "font_family" => @font_family,
+  }.merge(props)
+
+  delete_pre_draw_proc_by_name(name)
+  delete_post_draw_proc_by_name(name)
+
+  draw_order(items, indent_width, name) do |item|
+    props = default_props
+    props = props.merge(item.first.text_props) unless item.empty?
+    %Q[<span #{to_attrs(props)}>#{yield(item)}</span>]
+  end
+
+  space = @space * space_ratio
+  margin_with(:bottom => space)
+end
+
 slide_body = [Slide, Body]
 
 item_list_item = [ItemList, ItemListItem]
 
 match(*(slide_body + (item_list_item * 1))) do |items|
-  name = "item1"
-  
-  mark_width = screen_x(2)
-  mark_height = screen_y(2)
-  indent_width = mark_width * 3
-  color = "green"
-
-  delete_pre_draw_proc_by_name(name)
-  delete_post_draw_proc_by_name(name)
-  
-  draw_mark(items, indent_width, mark_width, mark_height, name) do
-    |item, canvas, x, y, w, h|
-    x -= mark_width
-    canvas.draw_rectangle(true, x, y, w, h, color)
-  end
-
-  space = @space * (3 / 4.0)
-  margin_with(:bottom => space)
+  setup_default_item_mark(items, "item1", 2, 2, (3 / 4.0),
+                          @default_item1_mark_color)
 end
 
 match(*(slide_body + (item_list_item * 2))) do |items|
-  name = "item2"
-  
-  mark_width = screen_x(1.5)
-  mark_height = screen_y(1.5)
-  indent_width = mark_width * 3
-  color = "blue"
-  
-  delete_pre_draw_proc_by_name(name)
-  delete_post_draw_proc_by_name(name)
-  
-  draw_mark(items, indent_width, mark_width, mark_height, name) do
-    |item, canvas, x, y, w, h|
-    x -= mark_width
-    canvas.draw_circle(true, x, y, w, h, color)
-  end
-
-  space = @space * (2 / 4.0)
-  margin_with(:bottom => space)
+  setup_default_item_mark(items, "item2", 1.5, 1.5, (2 / 4.0),
+                          @default_item2_mark_color)
 end
 
 match(*(slide_body + (item_list_item * 3))) do |items|
-  name = "item3"
-  
-  mark_width = screen_x(1.0)
-  mark_height = screen_y(1.0)
-  indent_width = mark_width * 3
-  color = "red"
-  
-  delete_pre_draw_proc_by_name(name)
-  delete_post_draw_proc_by_name(name)
-  
-  draw_mark(items, indent_width, mark_width, mark_height, name) do
-    |item, canvas, x, y, w, h|
-    x -= mark_width
-    canvas.draw_rectangle(true, x, y, w, h, color)
-  end
-
-  space = @space * (1 / 4.0)
-  margin_with(:bottom => space)
+  setup_default_item_mark(items, "item3", 1, 1, (1 / 4.0),
+                          @default_item3_mark_color)
 end
+
 
 enum_list_item = [EnumList, EnumListItem]
 
 match(*(slide_body + (enum_list_item * 1))) do |items|
-  name = "enum1"
-  
-  indent_width = screen_x(2)
-  default_props = {
-    "size" => @normal_font_size,
-    "font_family" => @font_family,
-  }
-
-  delete_pre_draw_proc_by_name(name)
-  delete_post_draw_proc_by_name(name)
-  
-  draw_order(items, indent_width, name) do |item|
-    props = default_props
-    props = props.merge(item.first.text_props) unless item.empty?
-    %Q[<span #{to_attrs(props)}>#{item.order}. </span>]
+  setup_default_enum_item_mark(items, 1, 2, (3 / 4.0),
+                               "size" => @normal_font_size) do |item|
+    "#{item.order}. "
   end
-
-  space = @space * (3 / 4.0)
-  margin_with(:bottom => space)
 end
 
 match(*(slide_body + (enum_list_item * 2))) do |items|
-  name = "enum2"
-  
-  indent_width = screen_x(1.5)
-  default_props = {
-    "size" => @small_font_size,
-    "font_family" => @font_family,
-  }
-
-  delete_pre_draw_proc_by_name(name)
-  delete_post_draw_proc_by_name(name)
-  
-  draw_order(items, indent_width, name) do |item|
-    props = default_props
-    props = props.merge(item.first.text_props) unless item.empty?
-    %Q[<span #{to_attrs(props)}>#{(?a + item.order - 1).chr}. </span>]
+  setup_default_enum_item_mark(items, 2, 1.5, (2 / 4.0),
+                               "size" => @small_font_size) do |item|
+    "#{(?a + item.order - 1).chr}. "
   end
-
-  space = @space * (2 / 4.0)
-  margin_with(:bottom => space)
 end
 
 match(*(slide_body + (enum_list_item * 3))) do |items|
-  name = "enum3"
-  
-  indent_width = screen_x(1)
-  default_props = {
-    "size" => @x_small_font_size,
-    "font_family" => @font_family,
-  }
-
-  delete_pre_draw_proc_by_name(name)
-  delete_post_draw_proc_by_name(name)
-  
-  draw_order(items, indent_width, name) do |item|
-    props = default_props
-    props = props.merge(item.first.text_props) unless item.empty?
-    %Q[<span #{to_attrs(props)}>#{(?A + item.order - 1).chr}. </span>]
+  setup_default_enum_item_mark(items, 3, 1, (1 / 4.0),
+                               "size" => @x_small_font_size) do |item|
+    "#{(?A + item.order - 1).chr}. "
   end
-
-  space = @space * (1 / 4.0)
-  margin_with(:bottom => space)
 end
 
 
 match(*(slide_body + enum_list_item + item_list_item)) do |items|
-  name = "enum-item1"
-  
-  mark_width = screen_x(1.5)
-  mark_height = screen_y(1.5)
-  indent_width = mark_width * 3
-  color = "#00ffff"
-
-  delete_pre_draw_proc_by_name(name)
-  delete_post_draw_proc_by_name(name)
-  
-  draw_mark(items, indent_width, mark_width, mark_height, name) do
-    |item, canvas, x, y, w, h|
-    x -= mark_width
-    canvas.draw_rectangle(true, x, y, w, h, color)
-  end
-
-  space = @space * (2 / 4.0)
-  margin_with(:bottom => space)
+  setup_default_item_mark(items, "enum-item1", 1.5, 1.5, (2 / 4.0),
+                          @default_enum_item1_mark_color)
 end
 
 match(*(slide_body + enum_list_item + (item_list_item * 2))) do |items|
-  name = "enum-item2"
-  
-  mark_width = screen_x(1)
-  mark_height = screen_y(1)
-  indent_width = mark_width * 3
-  color = "#ff00ff"
-
-  delete_pre_draw_proc_by_name(name)
-  delete_post_draw_proc_by_name(name)
-  
-  draw_mark(items, indent_width, mark_width, mark_height, name) do
-    |item, canvas, x, y, w, h|
-    x -= mark_width
-    canvas.draw_rectangle(true, x, y, w, h, color)
-  end
-
-  space = @space * (1 / 4.0)
-  margin_with(:bottom => space)
+  setup_default_item_mark(items, "enum-item2", 1, 1, (1 / 4.0),
+                          @default_enum_item2_mark_color)
 end
