86ba9bffffd236ae27240a6afc12447a7797db95
Kouhei Sutou <kou@cozmixng.org>
2007-06-24 08:29:13 +0000

    * auto-slide -> slide-show.

---
+++ lib/rabbit/theme/slide-show/auto-slide.rb
@@ -0,0 +1,24 @@
+theme_exit unless display?
+
+@auto_slide_span ||= 1000 * 60 # a minute
+@auto_slide_loop ||= false
+
+@@auto_slide_timeout_id ||= nil
+
+unless @@auto_slide_timeout_id.nil?
+  Gtk.timeout_remove(@@auto_slide_timeout_id)
+end
+
+@@auto_slide_timeout_id = Gtk.timeout_add(@auto_slide_span) do
+  if canvas.last_slide?
+    if @auto_slide_loop
+      canvas.activate("FirstSlide")
+      true
+    else
+      false
+    end
+  else
+    canvas.activate("NextSlide")
+    true
+  end
+end
