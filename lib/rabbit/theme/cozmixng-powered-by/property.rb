cd0c4c529dd9484408df16de28d59177bd0daa0d
Kouhei Sutou <kou@cozmixng.org>
2007-06-24 00:30:51 +0000

    * updated messages.

--- lib/rabbit/theme/cozmixng-powered-by/property.rb
+++ lib/rabbit/theme/cozmixng-powered-by/property.rb
@@ -1,6 +1,6 @@
 @category = N_("Toolkit")
 @title = N_("COZMIXNGPoweredBy")
-@abstract = N_("Powered-by-Cozmixng ad toolkit")
+@abstract = N_("Powered-by-COZMIXNG ad toolkit")
 @description = N_("Displays a text and images showing the presentation " \
                   "slides are powered by COZMIXNG and Rabbit at the foot of " \
                   "the title slide and of the last slide.")
