1eaa3f86a867fbc76e5cdca4f3f28598936dec33
Kouhei Sutou <kou@cozmixng.org>
2007-08-09 11:49:17 +0000

    * added missing color configurations into night-black theme.

--- lib/rabbit/theme/night-black/night-black.rb
+++ lib/rabbit/theme/night-black/night-black.rb
@@ -10,6 +10,14 @@
 @default_item1_mark_type = "circle"
 @default_item2_mark_color = "#a00000"
 @default_item2_mark_type = "circle"
+
+@preformatted_fill_color = "#101010"
+
+@block_quote_fill_color = "#101030"
+
+@table_head_fill_color = "#101030"
+@table_body_fill_color = "#101010"
+
 include_theme("default")
 
 @image_slide_number_show_text = true
