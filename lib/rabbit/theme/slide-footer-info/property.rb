d0aa5654687e34e583557d05ab0e93116d3b8baf
Kouhei Sutou <kou@cozmixng.org>
2008-07-30 12:46:46 +0000

    * update messages.

--- lib/rabbit/theme/slide-footer-info/property.rb
+++ lib/rabbit/theme/slide-footer-info/property.rb
@@ -1,19 +1,19 @@
 @category = N_("Toolkit")
 @title = N_("Slide Footer Info")
 @abstract = N_("Toolkit to display information at the footer")
-@description = N_("Displays information with a bar at the footer of slides.")
+@description = N_("Displays information with a line at the footer of slides.")
 @parameters = {
   "@slide_footer_info_line_color" => {
     :default => "#666",
-    :description => N_("Bar color."),
+    :description => N_("Line color."),
   },
   "@slide_footer_info_line_width" => {
     :default => "screen_y(0.1)",
-    :description => N_("Bar width."),
+    :description => N_("Line width."),
   },
   "@slide_footer_info_line_pattern" => {
     :default => N_("white <-> black gradation"),
-    :description => N_("Bar fill pattern. @slide_footer_info_line_color " \
+    :description => N_("Line fill pattern. @slide_footer_info_line_color " \
                        "is overridden by this parameter."),
   },
   "@slide_footer_info_text_size" => {
@@ -34,7 +34,7 @@
   },
   "@slide_footer_info_base_y" => {
     :default => "canvas.height - @margin_bottom",
-    :description => N_("Base y-axis position to stroke bar."),
+    :description => N_("Base y-axis position to stroke line."),
   },
   "@slide_footer_info_uninstall" => {
     :default => "false",
--- lib/rabbit/theme/slide-header-info/property.rb
+++ lib/rabbit/theme/slide-header-info/property.rb
@@ -1,19 +1,19 @@
 @category = N_("Toolkit")
 @title = N_("Slide Footer Info")
 @abstract = N_("Toolkit to display information at the header")
-@description = N_("Displays information with a bar at the header of slides.")
+@description = N_("Displays information with a line at the header of slides.")
 @parameters = {
   "@slide_header_info_line_color" => {
     :default => "#666",
-    :description => N_("Bar color."),
+    :description => N_("Line color."),
   },
   "@slide_header_info_line_width" => {
     :default => "screen_y(0.1)",
-    :description => N_("Bar width."),
+    :description => N_("Line width."),
   },
   "@slide_header_info_line_pattern" => {
     :default => N_("white <-> black gradation"),
-    :description => N_("Bar fill pattern. @slide_header_info_line_color " \
+    :description => N_("Line fill pattern. @slide_header_info_line_color " \
                        "is overridden by this parameter."),
   },
   "@slide_header_info_text_size" => {
@@ -34,7 +34,7 @@
   },
   "@slide_header_info_base_y" => {
     :default => "@margin_top",
-    :description => N_("Base y-axis position to stroke bar."),
+    :description => N_("Base y-axis position to stroke line."),
   },
   "@slide_header_info_uninstall" => {
     :default => "false",
