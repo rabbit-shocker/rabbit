44daa2f1b83c1810325bb62276afcadd81901b9d
Kouhei Sutou <kou@cozmixng.org>
2009-05-27 02:51:15 +0000

    * theme: slide-show: use (1 / allotted time) sec for each   slide by default.

--- lib/rabbit/theme/slide-show/slide-show.rb
+++ lib/rabbit/theme/slide-show/slide-show.rb
@@ -1,6 +1,12 @@
 theme_exit unless display?
 
-@slide_show_span ||= 1000 * 60 # a minute
+if canvas.allotted_time
+  default_slide_show_span = (canvas.allotted_time / canvas.slide_size.to_f).round
+else
+  default_slide_show_span = 60
+end
+default_slide_show_span *= 1000 # milliseconds -> seconds
+@slide_show_span ||= default_slide_show_span
 @slide_show_loop ||= false
 
 @@slide_show_timeout_id ||= nil
