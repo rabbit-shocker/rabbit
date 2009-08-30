9104abfb255c6488ee5ebb0246e327baff377437
Kouhei Sutou <kou@cozmixng.org>
2009-08-30 11:00:01 +0000

    * set margin for tortoise and here.

--- lib/rabbit/theme/pdf-tortoise-and-hare/pdf-tortoise-and-hare.rb
+++ lib/rabbit/theme/pdf-tortoise-and-hare/pdf-tortoise-and-hare.rb
@@ -2,10 +2,17 @@ include_theme("pdf")
 
 @image_slide_number_image = "mini-usa-taro.png"
 @image_slide_number_show_text = true
+
+@image_slide_number_margin_left = @margin_left
+@image_slide_number_margin_right = @margin_right
+@image_slide_number_margin_bottom = @margin_bottom
 include_theme("image-slide-number")
 
 canvas.title_slide['allotted-time'] = ENV["RABBIT_ALLOTTED_TIME"]
 if canvas.allotted_time
   @image_timer_image = "mini-kame-taro.png"
+  @image_timer_margin_left = @margin_left
+  @image_timer_margin_right = @margin_right
+  @image_timer_margin_bottom = @margin_bottom
   include_theme("image-timer")
 end
