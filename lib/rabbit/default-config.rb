70fd9c28151d8ff5014044c54d6f7c91143c63db
Kouhei Sutou <kou@cozmixng.org>
2009-10-03 13:18:27 +0000

    * add GEM_PATH/data/ to image path.

--- lib/rabbit/default-config.rb
+++ lib/rabbit/default-config.rb
@@ -6,5 +6,11 @@ module Rabbit
       ::Config::CONFIG["datadir"],
       File.join(".", "data"),
     ]
+    if Object.const_defined?(:Gem)
+      rabbit_gem_spec = Gem.loaded_specs["rabbit"]
+      if rabbit_gem_spec and rabbit_gem_spec.loaded?
+        IMAGE_PATH.unshift(File.join(rabbit_gem_spec.full_gem_path, "data"))
+      end
+    end
   end
 end
