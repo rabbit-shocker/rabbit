c79fa43bd723eae769e05d0ab1b8c6fb1e0850c8
Kouhei Sutou <kou@cozmixng.org>
2009-10-27 06:18:08 +0000

    * add a theme: lightning-monochrome

---
+++ lib/rabbit/theme/lightning-monochrome/lightning-monochrome.rb
@@ -0,0 +1,16 @@
+@slide_number_uninstall = true
+@default_headline_line_color = "black"
+@default_item1_mark_type = "check"
+@default_item2_mark_type = "circle"
+include_theme("default")
+
+@lightning_talk_proc_name = "lightning-default"
+include_theme("lightning-talk-toolkit")
+
+match(Slide) do |slides|
+  slides.each do |slide|
+    if slide.lightning_talk?
+      slide.lightning_talk
+    end
+  end
+end
---
+++ lib/rabbit/theme/lightning-monochrome/property.rb
@@ -0,0 +1,3 @@
+@category = N_("Theme")
+@title = N_("LightningTalk-Monochrome")
+@description = N_("Lightning Talk theme monochrome version")
