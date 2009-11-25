b660c8b6d96021db0d4dd94e6650c46914e68ece
Kouhei Sutou <kou@cozmixng.org>
2009-11-25 16:33:01 +0000

    * don't substitute newline in preoformatted block.

--- lib/rabbit/theme/newline-in-slides/newline-in-slides.rb
+++ lib/rabbit/theme/newline-in-slides/newline-in-slides.rb
@@ -1,3 +1,29 @@
 match("**") do |elements|
-  elements.substitute_newline
+  have_preformatted_in_parent = Proc.new do |element|
+    if element.nil?
+      false
+    elsif element.is_a?(PreformattedBlock)
+      true
+    else
+      have_preformatted_in_parent.call(element.parent)
+    end
+  end
+
+  have_preformatted_in_children = Proc.new do |element|
+    if element.nil?
+      false
+    elsif element.is_a?(PreformattedBlock)
+      true
+    elsif element.respond_to?(:elements)
+      element.elements.any? {|child| have_preformatted_in_children.call(child)}
+    else
+      false
+    end
+  end
+
+  elements.each do |element|
+    next if have_preformatted_in_parent.call(element)
+    next if have_preformatted_in_children.call(element)
+    element.substitute_newline
+  end
 end
