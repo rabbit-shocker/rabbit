a6f662a0ce753c1fd833e2e0debf2f64ac4934a0
Kouhei Sutou <kou@cozmixng.org>
2010-11-23 06:54:37 +0000

    theme: deafult-slide: support line width customize.

--- lib/rabbit/theme/default-slide/default-slide.rb
+++ lib/rabbit/theme/default-slide/default-slide.rb
@@ -1,4 +1,6 @@
 @default_headline_line_color ||= "#ff9933"
+@default_headline_line_params ||= {}
+@default_headline_line_width ||= 1
 
 match(Slide) do |slides|
   slides.each do |slide|
@@ -8,15 +10,24 @@ end
 
 match(Slide, HeadLine) do |headlines|
   name = "head-line"
-  
+
   delete_post_draw_proc_by_name(name)
 
   space = @space / 2.0
-  margin_with(:bottom => space * 3)
-  add_post_draw_proc(name) do |text, canvas, x, y, w, h, simulation|
+  top_space = space + @default_headline_line_width / 2.0
+  margin_with(:bottom => top_space * 1.5)
+  add_post_draw_proc(name) do |headline, canvas, x, y, w, h, simulation|
     unless simulation
-      canvas.draw_line(x, y + space, x + w, y + space,
-                       @default_headline_line_color)
+      if @default_headline_line_params.respond_to?(:call)
+        params = @default_headline_line_params.call(headline, canvas,
+                                                    x, y + top_space,
+                                                    w, h - top_space)
+      else
+        params = @default_headline_line_params
+      end
+      canvas.draw_line(x, y + top_space, x + w, y + top_space,
+                       @default_headline_line_color,
+                       params.merge(:line_width => @default_headline_line_width))
     end
     [x, y, w, h]
   end
