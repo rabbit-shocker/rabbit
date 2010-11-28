489e7f43b5fb77cc5dc463cd35a373a06b379ca6
Kouhei Sutou <kou@cozmixng.org>
2010-11-28 02:48:14 +0000

    * theme: slide-logo: make @slide_logo_width,   @slide_logo_height and @slide_logo_position customizable.

--- lib/rabbit/theme/slide-logo/slide-logo.rb
+++ lib/rabbit/theme/slide-logo/slide-logo.rb
@@ -14,16 +14,34 @@ match(SlideElement) do
   break if @slide_logo_image_uninstall
 
   loader = ImageLoader.new(find_file(@slide_logo_image))
-  loader.resize(@slide_logo_width, @slide_logo_height)
 
   add_pre_draw_proc(name) do |slide, canvas, x, y, w, h, simulation|
     unless simulation
-      if @slide_logo_position == :right
-        logo_x = canvas.width - loader.width
-      else
-        logo_x = 0
+      slide_logo_width = @slide_logo_width
+      slide_logo_height = @slide_logo_height
+      if slide_logo_width.respond_to?(:call)
+        slide_logo_width = slide_logo_width.call(slide, canvas)
       end
-      loader.draw(canvas, logo_x, 0)
+      if slide_logo_height.respond_to?(:call)
+        slide_logo_height = slide_logo_height.call(slide, canvas)
+      end
+      loader.resize(slide_logo_width, slide_logo_height)
+
+      case @slide_logo_position
+      when :right
+        logo_x = canvas.width - loader.width
+        logo_y = 0
+      when :left
+        logo_x = 0
+        logo_y = 0
+      else
+        if @slide_logo_position.respond_to?(:call)
+          logo_x, logo_y = @slide_logo_position.call(slide, canvas)
+        else
+          logo_x, logo_y = @slide_logo_position
+        end
+      end
+      loader.draw(canvas, logo_x, logo_y)
     end
     [x, y, w, h]
   end
