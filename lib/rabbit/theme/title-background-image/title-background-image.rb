d80635ce6f7848f61e49e425932241714f799fef
Kouhei Sutou <kou@cozmixng.org>
2011-01-15 16:25:27 +0000

    * title-background-image theme: support image file name in   slide property.   Suggested by kdmsnr. Thanks!!!

--- lib/rabbit/theme/title-background-image/title-background-image.rb
+++ lib/rabbit/theme/title-background-image/title-background-image.rb
@@ -1,7 +1,3 @@
-if @title_background_image.nil?
-  theme_exit("must specify @title_background_image!!")
-end
-
 @title_background_image_properties ||= {}
 default_title_backgroud_image_properties = {
   "as_large_as_possible" => true,
@@ -17,8 +13,13 @@ match(TitleSlide) do |slides|
       value = value.to_s if value.is_a?(Symbol)
       image_properties[key.to_s.gsub(/-/, "_")] = value
     end
+    background_image = slide["background-image"] || @title_background_image
+    if background_image.nil?
+      theme_exit("must specify 'background-image' slide property or " \
+                 "@title_background_image!!")
+    end
     properties = {
-      :file_name => find_file(@title_background_image),
+      :file_name => find_file(background_image),
       :proc_name => "title-background-image",
       :properties => image_properties,
     }
