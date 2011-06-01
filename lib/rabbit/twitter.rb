c667825884deabdd3964c457a2a607a61ec7e4b8
Kouhei Sutou <kou@clear-code.com>
2011-06-01 23:56:24 +0900

    [twitter] support stream filter.

--- lib/rabbit/twitter.rb
+++ lib/rabbit/twitter.rb
@@ -6,10 +6,16 @@ module Rabbit
     CONSUMER_KEY = "wT9WSC0afRw94fxUw0iIKw"
     CONSUMER_SECRET = "mwY35vfQfmWde9lZbyNNB15QzCq3k2VwGj3X1IAkQ8"
 
-    def initialize
+    def initialize(logger)
+      @logger = logger
       @oauth_parameters = nil
       @config_file_path = Pathname.new("~/.rabbit/twitter-oauth.yaml")
       @config_file_path = @config_file_path.expand_path
+      @listeners = []
+    end
+
+    def register_listener(&block)
+      @listeners << block
     end
 
     def setup
@@ -26,32 +32,37 @@ module Rabbit
       }
     end
 
-    def start_stream
+    def start_stream(*filters)
       setup if @oauth_parameters.nil?
       require 'twitter/json_stream'
       Thread.start do
         EventMachine.run do
           stream_options = {
             :oauth => @oauth_parameters,
+            :host => "stream.twitter.com",
+            :path => "/1/statuses/filter.json",
+            :method => "POST",
+            :filters => filters,
           }
           @stream = ::Twitter::JSONStream.connect(stream_options)
           @stream.each_item do |item|
-            p JSON.parse(item)
+            item = JSON.parse(item)
+            @listeners.each do |listener|
+              listener.call(item)
+            end
           end
 
           @stream.on_error do |message|
-            $stdout.print "error: #{message}\n"
-            $stdout.flush
+            @logger.error("[twitter] #{message}")
           end
 
           @stream.on_reconnect do |timeout, retries|
-            $stdout.print "reconnecting in: #{timeout} seconds\n"
-            $stdout.flush
+            @logger.info("[twitter][reconnect] #{timeout} seconds (#{retries})")
           end
 
           @stream.on_max_reconnects do |timeout, retries|
-            $stdout.print "Failed after #{retries} failed reconnects\n"
-            $stdout.flush
+            @logger.info("[twitter][max-reconnects] " +
+                         "Failed after #{retries} failed reconnects")
           end
         end
       end
