b95b3e85f5a51cd304b8b187530c088c229dd9d9
Kouhei Sutou <kou@clear-code.com>
2011-07-10 20:46:05 +0900

    [theme][rabbit-powered-by] disable powered by image by default.

--- lib/rabbit/theme/rabbit-powered-by/rabbit-powered-by.rb
+++ lib/rabbit/theme/rabbit-powered-by/rabbit-powered-by.rb
@@ -1,5 +1,5 @@
 add_image_path("rabbit-images")
 
-@powered_by_images = ["rabbit-banner.png"]
+# @powered_by_images = ["rabbit-banner.png"]
 @powered_by_text = "Powered by Rabbit #{VERSION} and COZMIXNG"
 include_theme("powered-by")
