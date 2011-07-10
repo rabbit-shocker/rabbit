5bfd851bbc81d27cd3c0cafbb77c49801dbb0794
Kouhei Sutou <kou@clear-code.com>
2011-07-10 14:00:54 +0900

    [theme][footer-comment] don't keep the last comment by default.
    To keep the last comment, set
    "@footer_comment_keep_last_comment = true" in your theme.

--- lib/rabbit/theme/footer-comment/footer-comment.rb
+++ lib/rabbit/theme/footer-comment/footer-comment.rb
@@ -16,6 +16,7 @@ proc_name = "footer-comment"
   :bottom => (@space * 0.5).ceil,
 }
 @footer_comment_min_display_time ||= 1
+@footer_comment_keep_last_comment ||= false
 
 match(SlideElement) do |slides|
   slides.delete_post_draw_proc_by_name(proc_name)
@@ -37,7 +38,11 @@ match(SlideElement) do |slides|
         text = Text.new(ERB::Util.h(content.strip.gsub("\n", " ")))
         if Time.now - redraw_time > @footer_comment_min_display_time
           redraw_time = Time.now
-          comments.shift if comments.size > 1
+          if @footer_comment_keep_last_comment
+            comments.shift if comments.size > 1
+          else
+            comments.shift
+          end
         end
         text.font @footer_comment_props
         text.padding_with @footer_comment_padding
