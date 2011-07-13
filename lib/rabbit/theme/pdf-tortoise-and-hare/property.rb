47f29e8e4b5c3670df70e5acad50c3262295887b
Kouhei Sutou <kou@clear-code.com>
2011-07-13 21:09:27 +0900

    [theme][pdf] merge pdf-tortoise-and-hare to pdf theme.

--- lib/rabbit/theme/pdf-tortoise-and-hare/property.rb
+++
@@ -1,3 +0,0 @@
-@category = N_("Theme")
-@title = N_("PDF with The Tortoise and The Hare")
-@description = N_("Theme for PDF with The Tortoise and The Hare")
--- lib/rabbit/theme/pdf/pdf.rb
+++ lib/rabbit/theme/pdf/pdf.rb
@@ -2,3 +2,19 @@ begin
   include_theme(canvas.title)
 rescue LoadError
 end
+
+if canvas.allotted_time
+  @image_slide_number_image = "mini-usa-taro.png"
+  @image_slide_number_show_text = true
+
+  @image_slide_number_margin_left = @margin_left
+  @image_slide_number_margin_right = @margin_right
+  @image_slide_number_margin_bottom = @margin_bottom
+  include_theme("image-slide-number")
+
+  @image_timer_image = "mini-kame-taro.png"
+  @image_timer_margin_left = @margin_left
+  @image_timer_margin_right = @margin_right
+  @image_timer_margin_bottom = @margin_bottom
+  include_theme("image-timer")
+end
