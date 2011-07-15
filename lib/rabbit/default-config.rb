da8bd7c90e4eb14bb6ccf35da21c95e3807f1850
Kouhei Sutou <kou@clear-code.com>
2011-07-15 22:37:14 +0900

    support RubyGems 1.8.5.
    Reported by okkez. Thanks!!!

--- lib/rabbit/default-config.rb
+++ lib/rabbit/default-config.rb
@@ -6,11 +6,19 @@ module Rabbit
       ::Config::CONFIG["datadir"],
       File.join(".", "data"),
     ]
-    if Object.const_defined?(:Gem)
+    append_path_from_gem = lambda do
+      return unless Object.const_defined?(:Gem)
       rabbit_gem_spec = Gem.loaded_specs["rabbit"]
-      if rabbit_gem_spec and rabbit_gem_spec.loaded?
+      return if rabbit_gem_spec.nil?
+      unless rabbit_gem_spec.respond_to?(:activated?)
+        def rabbit_gem_spec.activated?
+          loaded?
+        end
+      end
+      if rabbit_gem_spec.activated?
         IMAGE_PATH.unshift(File.join(rabbit_gem_spec.full_gem_path, "data"))
       end
     end
+    append_path_from_gem.call
   end
 end
