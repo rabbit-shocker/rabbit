e3aea40a06dc5fd4148aa92d4e03cd892c3c6b92
Kouhei Sutou <kou@clear-code.com>
2011-07-15 20:35:43 +0900

    [theme][title-slide-background-image] add.

---
+++ lib/rabbit/theme/title-slide-background-image/property.rb
@@ -0,0 +1,10 @@
+@category = N_("Toolkit")
+@title = N_("TitleSlideBackgroundImage")
+@abstract = N_("Toolkit to display an image as a background of the title slide")
+@description = N_("Displays an image as a background of the title slide.")
+@parameters = {
+  "@title_slide_background_image" => {
+    :default => N_("(Must be specified.)"),
+    :description => N_("Image file name."),
+  },
+}
