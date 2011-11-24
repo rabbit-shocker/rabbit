f1693f5bf8e5588b2ad8e9f5a62d8fa97884fd9c
Kouhei Sutou <kou@clear-code.com>
2011-11-24 12:00:09 +0900

    [theme][tag] make tag handler customizable.

--- lib/rabbit/theme/tag/tag.rb
+++ lib/rabbit/theme/tag/tag.rb
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 @tag_uninstall ||= false
+@tag_handlers ||= {}
 
 theme_exit if @tag_uninstall
 
 match("**", CustomTag) do |tags|
-  find_block_element = lambda do |tag|
+  find_outer_block = lambda do |tag|
     element = tag
     while element.inline_element?
       element = element.parent
@@ -13,7 +14,7 @@ match("**", CustomTag) do |tags|
     element
   end
 
-  find_markup_target = lambda do |tag|
+  find_target = lambda do |tag|
     if tag.elements.empty?
       tag.parent
     else
@@ -21,19 +22,39 @@ match("**", CustomTag) do |tags|
     end
   end
 
+  find_handler = lambda do |tag|
+    handler = @tag_handlers[tag.name]
+    return handler if handler
+    @tag_handlers.each do |key, value|
+      return value if key === tag.name
+    end
+    nil
+  end
+
   tags.each do |tag|
     case tag.name
     when "center"
-      find_block_element.call(tag).horizontal_centering = true
+      find_outer_block.call(tag).horizontal_centering = true
     when "right"
-      find_block_element.call(tag).align = "right"
+      find_outer_block.call(tag).align = "right"
+    when "small"
+      find_target.call(tag).prop_set("size", @small_font_size)
+    when "x-small"
+      find_target.call(tag).prop_set("size", @x_small_font_size)
+    when "large"
+      find_target.call(tag).prop_set("size", @large_font_size)
     when "x-large"
-      find_markup_target.call(tag).prop_set("size", @x_large_font_size)
+      find_target.call(tag).prop_set("size", @x_large_font_size)
     when "margin-top"
-      find_block_element.call(tag).margin_top += @space
+      find_outer_block.call(tag).margin_top += @space
     when "margin-bottom"
-      find_block_element.call(tag).margin_bottom += @space
+      find_outer_block.call(tag).margin_bottom += @space
+    else
+      handler = find_handler.call(tag)
+      if handler
+        handler.call(:target => find_target.call(tag),
+                     :outer_block => find_outer_block.call(tag))
+      end
     end
   end
 end
-
