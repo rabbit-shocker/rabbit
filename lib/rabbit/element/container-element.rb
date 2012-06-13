edf74eafedc022ef8c3e4efc72636967250add28
Kouhei Sutou <kou@clear-code.com>
2012-06-14 06:54:38 +0900

    Fix a bug that image isn't displayed
    Reported by TAKATSU Tomonari. Thanks!!!

--- lib/rabbit/element/container-element.rb
+++ lib/rabbit/element/container-element.rb
@@ -119,18 +119,22 @@ module Rabbit
         prev_is_inline = false
         @elements.each do |element|
           element.compile(canvas, x, y, w, h)
-          compile_horizontal(canvas, x, y, w, h)
+          compile_horizontal_element(element, canvas, x, y, w, h)
           x, y, w, h = element.draw(true)
         end
       end
 
       def compile_horizontal(canvas, x, y, w, h)
         @elements.each do |element|
-          if do_horizontal_centering? or element.do_horizontal_centering?
-            element.do_horizontal_centering(canvas, x, y, w, h)
-          else
-            element.reset_horizontal_centering(canvas, x, y, w, h)
-          end
+          compile_horizontal_element(element, canvas, x, y, w, h)
+        end
+      end
+
+      def compile_horizontal_element(element, canvas, x, y, w, h)
+        if do_horizontal_centering? or element.do_horizontal_centering?
+          element.do_horizontal_centering(canvas, x, y, w, h)
+        else
+          element.reset_horizontal_centering(canvas, x, y, w, h)
         end
       end
 
