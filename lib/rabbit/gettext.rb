f2922a6d8ba54d63c6da4f1f0d9750c65918babe
Kouhei Sutou <kou@clear-code.com>
2012-08-22 14:46:29 +0900

    Remove needless gettext fallback code

--- lib/rabbit/gettext.rb
+++ lib/rabbit/gettext.rb
@@ -1,45 +1,4 @@
-begin
-  require "rubygems"
-  raise LoadError if Gem.respond_to?(:_deprecated_all_load_paths)
-  require "gettext"
-  module GetText
-    if ::GetText::TextDomainManager.respond_to?(:textdomain)
-      alias _gettext gettext
-      module_function :_gettext
-      def gettext(msgid)
-        return msgid
-        if @@__textdomain[callersrc]
-          _gettext(msgid)
-        else
-          msgid
-        end
-      end
-    end
-  end
-rescue LoadError
-  module GetText
-
-    module_function
-    def bindtextdomain(*args)
-    end
-    
-    def gettext(msgid)
-      msgid
-    end
-  end
-
-  module Locale
-    module_function
-    def get
-      ["LC_ALL", "LC_MESSAGES", "LANG"].each do |env|
-        ret = ENV[env]
-        break if ret
-      end
-      ret = "C" unless ret
-      ret
-    end
-  end
-end
+require "gettext"
 
 module Rabbit
   module GetText
@@ -52,19 +11,8 @@ module Rabbit
       end
       charset ||= "UTF-8"
       ::GetText.bindtextdomain(DOMAIN, path, locale, charset)
-      if defined?(::GetText::TextDomainManager) and path
-        if ::GetText::TextDomainManager.respond_to?(:textdomain)
-          # workaround for Ruby-GetText 1.6.0 < 2.0.0
-          textdomain = ::GetText::TextDomainManager.textdomain(DOMAIN)
-          locale_paths = ["#{path}/%{locale}/LC_MESSAGES/%{name}.mo",
-                        "#{path}/%{locale}/%{name}.mo"]
-          textdomain.locale_paths.concat(locale_paths)
-          locale ||= textdomain.current_locale || Locale.get
-          textdomain.set_locale(locale, true)
-        end
-      end
     end
-    
+
     def _(msgid)
       ::GetText.gettext(msgid)
     end
