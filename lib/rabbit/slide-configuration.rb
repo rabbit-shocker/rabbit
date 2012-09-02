f6d170fa3b00e156f19563442f93e1a9256d1903
Kouhei Sutou <kou@clear-code.com>
2012-09-02 17:31:24 +0900

    Extract slide configuration

---
+++ lib/rabbit/slide-configuration.rb
@@ -0,0 +1,99 @@
+# Copyright (C) 2012  Kouhei Sutou <kou@cozmixng.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+require "yaml"
+
+require "rabbit/gettext"
+require "rabbit/author-configuration"
+require "rabbit/path-manipulatable"
+
+module Rabbit
+  class SlideConfiguration
+    include GetText
+    include PathManipulatable
+
+    attr_accessor :logger
+    attr_accessor :id, :base_name, :tags, :presentation_date, :version
+    attr_reader :author
+    def initialize(logger=nil)
+      @logger = logger || Logger.default
+      @id = nil
+      @base_name = nil
+      @tags = []
+      @presentation_date = nil
+      @version = nil
+      @author = nil
+    end
+
+    def load
+      return unless File.exist?(path)
+      conf = YAML.load(File.read(path))
+      merge!(conf)
+    rescue
+      format = _("Failed to read slide configuration: %s: %s")
+      @logger.error(format % [path, $!.message])
+    end
+
+    def save
+      create_file(path) do |conf_file|
+        conf_file.print(to_yaml)
+      end
+    rescue
+      format = _("Failed to write slide configuration: %s: %s")
+      @logger.error(format % [path, $!.message])
+    end
+
+    def merge!(conf)
+      @id                = conf["id"]
+      @base_name         = conf["base_name"]
+      @tags              = conf["tags"]
+      @presentation_date = conf["presentation_date"]
+      @version           = conf["version"]
+
+      @author = AuthorConfiguration.new
+      @author.merge!(conf["author"] || {})
+    end
+
+    def to_hash
+      config = {
+        "id"                => @id,
+        "base_name"         => @base_name,
+        "tags"              => @tags,
+        "presentation_date" => @presentation_date,
+        "version"           => @version,
+      }
+      config["author"] = @author.to_hash if @author
+      config
+    end
+
+    def to_yaml
+      to_hash.to_yaml
+    end
+
+    def gem_name
+      "#{gem_name_prefix}-#{@author.rubygems_user}-#{@id}"
+    end
+
+    def path
+      "config.yaml"
+    end
+
+    private
+    def gem_name_prefix
+      "rabbit-slide"
+    end
+  end
+end
---
+++ test/test-slide-configuration.rb
@@ -0,0 +1,44 @@
+# Copyright (C) 2012  Kouhei Sutou <kou@cozmixng.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+require "rabbit/slide-configuration"
+
+class TestSlideConfiguration < Test::Unit::TestCase
+  def setup
+    @slide = Rabbit::SlideConfiguration.new
+  end
+
+  def test_merge!
+    author_conf = {
+      "markup_language"    => "rd",
+      "name"               => "Kouhei Sutou",
+      "email"              => "kou@cozmixng.org",
+      "rubygems_user"      => "kou",
+      "slideshare_user"    => "kou",
+      "speaker_deck_user"  => "kou",
+    }
+    conf = {
+      "id"                => "sprk2012",
+      "base_name"         => "how-to-make-clear-code",
+      "tags"              => ["rabbit", "sprk2012", "clear code"],
+      "presentation_date" => ["2012/09/16"],
+      "version"           => "2012.09.16",
+      "author"            => author_conf,
+    }
+    @slide.merge!(conf)
+    assert_equal(conf, @slide.to_hash)
+  end
+end
