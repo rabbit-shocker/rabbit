918dbfc3964d6dd5d99387feaa3b603b8acf160c
Kouhei Sutou <kou@clear-code.com>
2012-09-06 23:21:03 +0900

    title-on-image-toolkit: support title background

--- lib/rabbit/theme/title-on-image-toolkit/title-on-image-toolkit.rb
+++ lib/rabbit/theme/title-on-image-toolkit/title-on-image-toolkit.rb
@@ -2,11 +2,13 @@
 @title_on_image_color ||= "white"
 @title_on_image_shadow_color ||= "#0009"
 @title_on_image_background_color ||= "black"
+@title_on_image_title_background_color ||= "#ccc6"
 @title_on_image_params = {
   :proc_name => @title_on_image_proc_name,
   :color => @title_on_image_color,
   :shadow_color => @title_on_image_shadow_color,
   :background_color => @title_on_image_background_color,
+  :title_background_color => @title_on_image_title_background_color,
 }
 
 def setup_title_on_image_slide(slide)
@@ -32,6 +34,7 @@ def setup_title_on_image_slide(slide)
       proc_name = params[:proc_name]
       shadow_color = params[:shadow_color]
       background_color = params[:background_color]
+      title_background_color = params[:title_background_color]
 
       orig_x = orig_y = orig_w = orig_h = nil
       add_pre_draw_proc(proc_name) do |canvas, x, y, w, h, simulation|
@@ -53,20 +56,27 @@ def setup_title_on_image_slide(slide)
       add_post_draw_proc(proc_name) do |canvas, x, y, w, h, simulation|
         base_h = orig_h / 2 - headline.height / 2
         base_y = orig_y + base_h
-        unless simulation
-          shadow_headline = headline.clone
-          shadow_headline.font(:color => nil)
-          shadow_layout = canvas.make_layout(shadow_headline.markuped_text)
-          shadow_layout.set_width(w * Pango::SCALE)
-          shadow_layout.set_alignment(Pango::Layout::ALIGN_CENTER)
-
-          font_size = headline.pixel_font_size
-          move_x = @applier.screen_x(font_size.to_f / @applier.screen_size(10))
-          move_y = @applier.screen_y(font_size.to_f / @applier.screen_size(20))
-          canvas.draw_layout(shadow_layout, orig_x + move_x, base_y + move_y,
-                             shadow_color)
-        end
         headline.show do
+          unless simulation
+            background_y = base_y - headline.padding_top
+            background_h = headline.height
+            canvas.draw_rectangle(true,
+                                  0,            background_y,
+                                  canvas.width, background_h,
+                                  title_background_color)
+
+            shadow_headline = headline.clone
+            shadow_headline.font(:color => nil)
+            shadow_layout = canvas.make_layout(shadow_headline.markuped_text)
+            shadow_layout.set_width(w * Pango::SCALE)
+            shadow_layout.set_alignment(Pango::Layout::ALIGN_CENTER)
+
+            font_size = headline.pixel_font_size
+            move_x = @applier.screen_x(font_size.to_f / @applier.screen_size(10))
+            move_y = @applier.screen_y(font_size.to_f / @applier.screen_size(20))
+            canvas.draw_layout(shadow_layout, orig_x + move_x, base_y + move_y,
+                               shadow_color)
+          end
           headline.draw_element(canvas,
                                 orig_x, base_y, orig_w, base_h,
                                 simulation)
