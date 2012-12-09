a523fce4a529a18598761b9b0200011d9fac1d9f
Kouhei Sutou <kou@clear-code.com>
2012-12-09 16:53:18 +0900

    Remove needless licenses accessor from Rakefile template
    It should be specified in config.yaml.

--- lib/rabbit/command/rabbit-slide.rb
+++ lib/rabbit/command/rabbit-slide.rb
@@ -381,7 +381,6 @@ require "rabbit/task/slide"
 # Edit ./config.yaml to customize meta data
 
 Rabbit::Task::Slide.new do |task|
-  # task.spec.licenses = ["CC BY-SA 3.0"]
   # task.spec.files += Dir.glob("doc/**/*.*")
   # task.spec.files -= Dir.glob("private/**/*.*")
   # task.spec.add_runtime_dependency("YOUR THEME")
