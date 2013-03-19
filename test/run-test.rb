f1a4c03bc875604cc427601f5d459e04f8572fa8
Masafumi Yokoyama <myokoym@gmail.com>
2013-03-19 20:20:56 +0900

    test: add current directory to LOAD_PATH

--- test/run-test.rb
+++ test/run-test.rb
@@ -16,6 +16,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+$LOAD_PATH.unshift(File.expand_path("."))
 $LOAD_PATH.unshift(File.join(File.expand_path("."), "lib"))
 $LOAD_PATH.unshift(File.join(File.expand_path("."), "test"))
 
