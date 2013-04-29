501f17e8fd3b574fa753cc9c953d8d9a56f6e66e
Kouhei Sutou <kou@clear-code.com>
2013-04-29 16:58:33 +0900

    Bump version
    2.0.7 ->
    2.0.8

--- lib/rabbit/version.rb
+++ lib/rabbit/version.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 Kouhei Sutou <kou@cozmixng.org>
+# Copyright (C) 2012-2013  Kouhei Sutou <kou@cozmixng.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,5 +15,5 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 module Rabbit
-  VERSION = "2.0.7"
+  VERSION = "2.0.8"
 end
