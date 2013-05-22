6f1968fb45561d3e313a4638ed3c93cb169f8b50
Kouhei Sutou <kou@clear-code.com>
2013-05-22 21:36:21 +0900

    Indent

--- doc/_plugins/converters/markdown.rb
+++ doc/_plugins/converters/markdown.rb
@@ -2,14 +2,14 @@ require 'jekyll/converters/markdown'
 
 module Jekyll
   module Converters
-  class Markdown
-    alias_method :convert_without_container, :convert
-    def convert(content)
-      snippet = convert_without_container(content)
-      snippet = "<section class=\"contents\">\n#{snippet}\n</section>"
-      snippet = "<div id=\"main\">\n#{snippet}\n</div>"
-      snippet
+    class Markdown
+      alias_method :convert_without_container, :convert
+      def convert(content)
+        snippet = convert_without_container(content)
+        snippet = "<section class=\"contents\">\n#{snippet}\n</section>"
+        snippet = "<div id=\"main\">\n#{snippet}\n</div>"
+        snippet
+      end
     end
-  end
   end
 end
