68ca1b5e73f01c89bf92a65ee136effb58e86f42
Kouhei Sutou <kou@clear-code.com>
2013-11-19 21:59:02 +0900

    rabbit-slide: add "tag" Rake task
    We already assumed that user may use Git as VCS. We already created
    .gitignore.

--- lib/rabbit/command/rabbit-slide.rb
+++ lib/rabbit/command/rabbit-slide.rb
@@ -375,15 +375,23 @@ EOD
 
       def generate_rakefile
         create_file("Rakefile") do |rakefile|
-          rakefile.puts(<<-EOR)
+          rakefile.puts(<<-'EOR')
 require "rabbit/task/slide"
 
 # Edit ./config.yaml to customize meta data
 
+spec = nil
 Rabbit::Task::Slide.new do |task|
+  spec = task.spec
   # task.spec.files += Dir.glob("doc/**/*.*")
   # task.spec.files -= Dir.glob("private/**/*.*")
   # task.spec.add_runtime_dependency("YOUR THEME")
+end
+
+desc "Tag #{spec.version}"
+task :tag do
+  sh("git", "tag", "-a", spec.version.to_s, "-m", "Publish #{spec.version}")
+  sh("git", "push", "--tags")
 end
 EOR
         end
