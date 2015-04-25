b48c872571cd4c57fcf0ab85a463b5f80ed4b46c
Kouhei Sutou <kou@clear-code.com>
2015-04-25 15:17:41 +0900

    Use String for stock ID

--- lib/rabbit/stock.rb
+++ lib/rabbit/stock.rb
@@ -6,7 +6,7 @@ module Rabbit
   module Stock
     include GetText
 
-    RABBIT = :rabbit_rabbit
+    RABBIT = "rabbit-rabbit"
 
     @@loaded = false
 
@@ -14,14 +14,14 @@ module Rabbit
     def init(canvas)
       return if @@loaded
       @@loaded = true
-      Gtk::Stock.add(RABBIT, N_("Rabbit"))
+      Gtk::Stock.add(RABBIT.to_sym, N_("Rabbit"))
       begin
         image_theme = Theme::Searcher.find_theme("rabbit-images", true)
         file = Theme::Searcher.find_file("lavie-icon.png", [image_theme])
         loader = ImageLoader.new(file)
         loader.resize(32, 32)
         factory = Gtk::IconFactory.new
-        factory.add(RABBIT.to_s, Gtk::IconSet.new(loader.pixbuf))
+        factory.add(RABBIT, Gtk::IconSet.new(loader.pixbuf))
         factory.add_default
       rescue LoadError
         canvas.logger.warn($!)
