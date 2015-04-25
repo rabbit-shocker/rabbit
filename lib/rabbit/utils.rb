585ea908b660905e45ae4730b7f1fb245c5d9d97
Kouhei Sutou <kou@clear-code.com>
2015-04-25 23:50:08 +0900

    Use width and height

--- lib/rabbit/utils.rb
+++ lib/rabbit/utils.rb
@@ -1,3 +1,19 @@
+# Copyright (C) 2004-2015  Kouhei Sutou <kou@cozmixng.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 require 'tempfile'
 require 'nkf'
 
@@ -186,7 +202,8 @@ module Rabbit
       num = screen.get_monitor(window)
       monitor = screen.get_monitor_geometry(num)
       window_x, window_y = window.origin
-      window_width, window_height = window.size
+      window_width  = window.width
+      window_height = window.height
       target_width, target_height = target.size_request
 
       args = [window_x, window_y, window_width, window_height]
