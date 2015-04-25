188bbf24d0983eb38e4b7675f90cc7e6c337b3a7
Kouhei Sutou <kou@clear-code.com>
2015-04-25 23:54:52 +0900

    Use symbol

--- lib/rabbit/menu.rb
+++ lib/rabbit/menu.rb
@@ -1,3 +1,19 @@
+# Copyright (C) 2004-2015  Kouhei Sutou <kou@cozmixng.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 require "erb"
 require "stringio"
 
@@ -131,8 +147,7 @@ module Rabbit
       label = _(category)
       action = Gtk::Action.new(name, label, nil, nil)
       @theme_actions.add_action(action)
-      @merge.add_ui(@theme_merge_id, path, category, name,
-                    Gtk::UIManager::MENU, false)
+      @merge.add_ui(@theme_merge_id, path, category, name, :menu, false)
     end
 
     def theme_menu_add_theme(prefix, path, entry, canvas)
