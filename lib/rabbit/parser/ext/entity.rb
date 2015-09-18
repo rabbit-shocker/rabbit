dd5f046ae1d7f58e6f5e7ca9c21902b61ad19f5e
takiy33 <takiy33@gmail.com>
2015-09-19 08:47:28 +0900

    Remove duplicate Dagger key

--- lib/rabbit/parser/ext/entity.rb
+++ lib/rabbit/parser/ext/entity.rb
@@ -2405,8 +2405,6 @@ module Rabbit
           "copysr" => "&#x02117;",
           # =ballot cross
           "cross" => "&#x02717;",
-          # /ddagger B: =double dagger
-          "Dagger" => "&#x02021;",
           # /dagger B: =dagger
           "dagger" => "&#x02020;",
           # =hyphen (true graphic)
