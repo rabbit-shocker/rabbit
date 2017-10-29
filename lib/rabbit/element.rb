0c8deae7011a15e5774abb8345c105bcffeee657
Kouhei Sutou <kou@clear-code.com>
2017-10-29 23:42:47 +0900

    Require elements manually

--- lib/rabbit/element.rb
+++ lib/rabbit/element.rb
@@ -1,8 +1,35 @@
-require 'rabbit/utils'
+# Copyright (C) 2017  Kouhei Sutou <kou@cozmixng.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-module Rabbit
-  module Element
-    dir = ::File.join("rabbit", "element")
-    Utils.require_files_under_directory_in_load_path(dir)
-  end
-end
+require "rabbit/element/block"
+require "rabbit/element/description-list"
+require "rabbit/element/enum-list"
+require "rabbit/element/footnote"
+require "rabbit/element/image-title-slide"
+require "rabbit/element/image"
+require "rabbit/element/index-slide"
+require "rabbit/element/item-list"
+require "rabbit/element/method-list"
+require "rabbit/element/poppler-slide"
+require "rabbit/element/poppler-title-slide"
+require "rabbit/element/preformatted"
+require "rabbit/element/slide"
+require "rabbit/element/syntax-highlighting"
+require "rabbit/element/table"
+require "rabbit/element/tag"
+require "rabbit/element/text"
+require "rabbit/element/title-slide"
+require "rabbit/element/video"
