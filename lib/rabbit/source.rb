8cc776dc366962a1ed2953597b6173c911f37252
Kouhei Sutou <kou@clear-code.com>
2017-10-29 23:48:57 +0900

    Require sources manually
    Unmaintained sources are removed:
    
      * Hiki
      * SlideShare

--- lib/rabbit/source.rb
+++ lib/rabbit/source.rb
@@ -1,14 +1,30 @@
-require "rabbit/utils"
+# Copyright (C) 2017  Kouhei Sutou <kou@cozmixng.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+require "rabbit/source/argf"
+require "rabbit/source/file"
+require "rabbit/source/memory"
+require "rabbit/source/uri"
 
 module Rabbit
   module Source
-    extend Utils
-
-    dir = ::File.join("rabbit", "source")
-    require_files_under_directory_in_load_path(dir)
-
-    def self.types
-      collect_classes_under_module(self)
+    class << self
+      def types
+        Utils.collect_classes_under_module(self)
+      end
     end
   end
 end
