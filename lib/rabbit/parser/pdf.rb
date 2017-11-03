6a64a49eb264ae9a1bc95a9176b02510a36a5542
Kouhei Sutou <kou@clear-code.com>
2017-11-03 10:19:22 +0900

    Add workaround for Ruby/Poppler 3.1.9

--- lib/rabbit/parser/pdf.rb
+++ lib/rabbit/parser/pdf.rb
@@ -1,3 +1,19 @@
+# Copyright (C) 2007-2017  Kouhei Sutou <kou@cozmixng.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 require "tempfile"
 
 require "poppler"
@@ -26,13 +42,19 @@ module Rabbit
 
       include Element
       def parse
-        doc = Poppler::Document.new(@source.read)
+        # Workaround for Ruby/Poppler 3.1.9
+        pdf_file = Tempfile.new(["rabbit", "pdf"])
+        pdf_file.write(@source.read)
+        pdf_file.flush
+        doc = Poppler::Document.new(:path => pdf_file.path)
+        pdf_file.close!
 
-        title_page, *rest = doc.to_a
-
-        @canvas << PopplerTitleSlide.new(title_page, doc)
-        rest.each do |page|
-          @canvas << PopplerSlide.new(page)
+        doc.each_with_index do |page, i|
+          if i.zero?
+            @canvas << PopplerTitleSlide.new(page, doc)
+          else
+            @canvas << PopplerSlide.new(page)
+          end
         end
       end
     end
