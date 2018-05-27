d02352d91d54388a59ee39d9b4d696937cbedade
Kouhei Sutou <kou@clear-code.com>
2018-05-27 10:27:24 +0900

    logger gui: use options style

--- lib/rabbit/logger/gui.rb
+++ lib/rabbit/logger/gui.rb
@@ -60,7 +60,7 @@ module Rabbit
       end
 
       def append(text, *tags)
-        iter = @buffer.get_iter_at_offset(-1)
+        iter = @buffer.get_iter_at(offset: -1)
         @buffer.insert_with_tags(iter, text.encode("UTF-8"), *tags)
       end
 
