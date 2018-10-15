53c2034c885a2b500bcd52c14ab456c5b33818c1
Kouhei Sutou <kou@clear-code.com>
2018-10-15 15:46:30 +0900

    Use double quote for string literal

--- lib/rabbit/html/generator.rb
+++ lib/rabbit/html/generator.rb
@@ -1,15 +1,31 @@
-require 'erb'
-require 'fileutils'
+# Copyright (C) 2018  Kouhei Sutou <kou@cozmixng.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+require "erb"
+require "fileutils"
 
 begin
-  require 'rss/maker'
+  require "rss/maker"
 rescue LoadError
 end
 
-require 'rabbit/rabbit'
-require 'rabbit/front'
-require 'rabbit/utils'
-require 'rabbit/keys'
+require "rabbit/rabbit"
+require "rabbit/front"
+require "rabbit/utils"
+require "rabbit/keys"
 
 module Rabbit
   module HTML
