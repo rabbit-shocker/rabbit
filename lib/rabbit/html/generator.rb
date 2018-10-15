4ff29baec1da2a90b807525aeb609c08703846c7
Kouhei Sutou <kou@clear-code.com>
2018-10-15 15:48:15 +0900

    Use double quote for string literal

--- lib/rabbit/html/generator.rb
+++ lib/rabbit/html/generator.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2018  Kouhei Sutou <kou@cozmixng.org>
+# Copyright (C) 2005-2018  Kouhei Sutou <kou@cozmixng.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
