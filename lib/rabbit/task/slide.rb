6706dc18667bac2b1338cdfc5ff3a3c8db6df094
Sutou Kouhei <kou@cozmixng.org>
2021-10-17 06:18:23 +0900

    Fix style

--- lib/rabbit/task/slide.rb
+++ lib/rabbit/task/slide.rb
@@ -65,7 +65,7 @@ module Rabbit
           spec.summary = readme_parser.title || "TODO"
           spec.description = readme_parser.description || "TODO"
           spec.licenses = @slide.licenses
-          spec.metadata['source_code_uri'] = @slide.source_code_uri
+          spec.metadata["source_code_uri"] = @slide.source_code_uri
 
           slide_conf_path = @slide.path
           spec.files = [".rabbit", slide_conf_path, "Rakefile"]
