0bc45f0c038854390983f21afdd32c28b42732f2
Sutou Kouhei <kou@clear-code.com>
2023-07-02 07:06:08 +0900

    image mermaid: add missing GetText include
    GitHub: fix GH-153
    
    Patch by Mamoru TASAKA. Thanks!!!

--- lib/rabbit/image/mermaid.rb
+++ lib/rabbit/image/mermaid.rb
@@ -14,15 +14,16 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+require "rabbit/gettext"
 require "rabbit/image/pdf"
 
 module Rabbit
   module ImageManipulable
     class Mermaid < Base
+      include GetText
+      include SystemRunner
 
       unshift_loader(self)
-
-      include SystemRunner
 
       class << self
         def match?(filename)
