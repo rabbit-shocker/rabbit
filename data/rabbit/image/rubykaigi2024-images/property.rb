7fe4047bb944a6b90a87efbb371464ec556289bb
HASUMI Hitoshi <hasumikin@gmail.com>
2024-04-15 17:12:05 +0900

    Update data/rabbit/image/rubykaigi2024-images/property.rb
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>

--- data/rabbit/image/rubykaigi2024-images/property.rb
+++ data/rabbit/image/rubykaigi2024-images/property.rb
@@ -1,4 +1,4 @@
 @category = N_("Image")
-@title = N_("RubyKaigi2024Image")
-@abstract = N_("Images related to RubyKaigi2024")
-@description = N_("Provides images related to RubyKaigi2024.")
+@title = N_("RubyKaigi 2024 image")
+@abstract = N_("Images related to RubyKaigi 2024")
+@description = N_("Provides images related to RubyKaigi 2024. They are licensed under CC BY 4.0 by RubyKaigi 2024 Team.")
