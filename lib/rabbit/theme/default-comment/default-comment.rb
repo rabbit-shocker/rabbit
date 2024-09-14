84b3b65b84d432bf97e6f6e62ef59db0e4a14582
Sutou Kouhei <kou@clear-code.com>
2024-09-14 10:03:49 +0900

    Drop support for Clutter
    Because Clutter is deprecated.

--- lib/rabbit/theme/default-comment/default-comment.rb
+++ lib/rabbit/theme/default-comment/default-comment.rb
@@ -1,9 +1,5 @@
 if canvas.comment_theme
   include_theme(canvas.comment_theme)
 else
-  if display? and canvas.renderer.widget.class.name == "Clutter::GtkEmbed"
-    include_theme("clutter-comment")
-  else
-    include_theme("footer-comment")
-  end
+  include_theme("footer-comment")
 end
