69e48082d74d3755d39425a219f218a29c3974cc
Sutou Kouhei <kou@clear-code.com>
2025-06-11 14:16:25 +0900

    Require rabbit/utils explicitly

--- lib/rabbit/image/base.rb
+++ lib/rabbit/image/base.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2004-2022  Sutou Kouhei <kou@cozmixng.org>
+# Copyright (C) 2004-2025  Sutou Kouhei <kou@cozmixng.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,6 +21,7 @@ require "gdk_pixbuf2"
 
 require "rabbit/image-data-loader"
 require "rabbit/properties"
+require "rabbit/utils"
 
 module Rabbit
   module ImageManipulable
