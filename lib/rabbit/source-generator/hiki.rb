666d2f5a0d6e091e6243f02744ae53d152374377
Sutou Kouhei <kou@clear-code.com>
2025-07-03 11:25:05 +0900

    Fix path

--- lib/rabbit/source-generator/hiki.rb
+++ lib/rabbit/source-generator/hiki.rb
@@ -14,7 +14,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-require_relative "rabbit"
+require_relative "../rabbit"
 
 module Rabbit
   module SourceGenerator
